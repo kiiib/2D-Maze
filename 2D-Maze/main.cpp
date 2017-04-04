0e1e62832309a7b7eb2a7ff6d01906b15fe70b26
Jian-Bin Liu <billyljb@gmail.com>
2017-04-04 12:23:14 +0800

    update

---
+++ 2D-Maze/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Angel.h"
+using namespace std;
+
+#define WIDTH 10	// Maze's Width
+#define HEIGHT 10	// Maze's Height
+
+enum Wall{
+	UP = 1,
+	DOWN = 4,
+	LEFT = 2,
+	RIGHT = 8
+};
+int main() {
+	cout << "Hey" << endl;
+	srand
+	getchar();
+	return 0;
+}
